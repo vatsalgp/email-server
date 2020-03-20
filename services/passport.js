322c902310a5ee2daa3c343a05266dfd14786c3c
vatsalgp <vatsalgupta280@gmail.com>
2020-03-21 00:04:49 +0530

    changed to async await

--- services/passport.js
+++ services/passport.js
@@ -22,16 +22,14 @@ const GSOptions = {
     proxy: true // Trusts Proxy allowing us to use https over heroku proxy server
 };
 
-const GS = new GoogleStrategy(GSOptions, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser)
-            done(null, existingUser);
-        else {
-            new User({ googleId: profile.id })
-                .save()
-                .then(user => done(null, user));
-        }
-    });
-});
+const GSVerify = async (accessToken, refreshToken, profile, done) => {
+    const googleId = profile.id;
+    let user = await User.findOne({ googleId });
+    if (!user)
+        user = await new User({ googleId }).save();
+    done(null, user);
+};
+
+const GS = new GoogleStrategy(GSOptions, GSVerify);
 
 passport.use(GS);

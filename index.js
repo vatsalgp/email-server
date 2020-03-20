f808160ee692a13f6581144d629c9ef211d238c1
vatsalgp <vatsalgupta280@gmail.com>
2020-03-20 18:55:10 +0530

    Revert "added cors"
    This reverts commit 3346b02da6fd81d88c132733cb8da865ebff2def.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = require("express")();
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const cors = require("cors");
 const keys = require("./config/keys");
 const router = require("./routes/authRoutes");
 
@@ -20,16 +19,7 @@ const cookieOptions = {
     keys: [keys.cookieKey]
 };
 
-const corsOptions = {
-    origin: 'https://emaily.netlify.com',//No ending slash
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
 //Middlewares
-if (process.env.NODE_ENV === "production")
-    app.use(cors(corsOptions));
-else
-    app.use(cors());
 app.use(cookieSession(cookieOptions));
 app.use(passport.initialize());
 app.use(passport.session());

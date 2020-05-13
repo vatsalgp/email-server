377a2792f8650d5fd39ccb0c391a653289e521c1
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-05-13 14:32:23 +0530

    added try catch block to actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,25 +1,35 @@
 import axios from "axios";
 
 export const fetchUser = () => async dispatch => {
-    const response = await axios.get("/api/current_user");
-    if (response.data) {
-        const { credits, googleId } = response.data;
-        dispatch({
-            type: "FETCH_USER",
-            payload: { credits, googleId, isSignedIn: true }
-        });
-    } else {
-        dispatch({
-            type: "FETCH_USER",
-            payload: { isSignedIn: false }
-        });
+    try {
+        const response = await axios.get("/api/current_user");
+        if (response.data) {
+            const { credits, googleId } = response.data;
+            dispatch({
+                type: "FETCH_USER",
+                payload: { credits, googleId, isSignedIn: true }
+            });
+        } else {
+            dispatch({
+                type: "FETCH_USER",
+                payload: { isSignedIn: false }
+            });
+        }
+    }
+    catch (error) {
+        console.log(error);
     }
 };
 
 export const handleToken = token => async dispatch => {
-    const response = await axios.post("/api/stripe", token);
-    dispatch({
-        type: "FETCH_USER",
-        payload: response.data
-    });
+    try {
+        const response = await axios.post("/api/stripe", token);
+        dispatch({
+            type: "FETCH_USER",
+            payload: response.data
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
 };

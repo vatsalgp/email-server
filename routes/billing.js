e8de63b9db99876cbeb8628dcfd943fcb00caa98
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-05-14 20:29:56 +0530

    moved createCharge outside

--- routes/billing.js
+++ routes/billing.js
@@ -1,23 +1,6 @@
 const requireLogin = require("../middlewares/requireLogin");
-const keys = require("../config/keys");
-const stripe = require("stripe")(keys.stripeSecretKey);
+const createCharge = require("../services/createCharge");
 
 module.exports = app => {
-    const createCharge = async (req, res) => {
-        //req.body -> token; req.user -> user 
-        const charge = await stripe.charges.create({
-            amount: 500,
-            currency: "usd",
-            description: "$5 for 5 credits",
-            source: req.body.id // Transaction id
-        });
-
-        req.user.credits += 5;
-
-        const user = await req.user.save();
-
-        res.send(user);
-    };
-
     app.post("/api/stripe", requireLogin, createCharge);
 };
